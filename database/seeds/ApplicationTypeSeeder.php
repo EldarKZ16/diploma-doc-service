05ca87fda29cec553c416578f9473c10231a0f69
EldarKZ16 <aeldar0016@gmail.com>
2021-01-31 16:26:47 +0600

    fix migrate_db_and_seed.sh

--- database/seeds/ApplicationTypeSeeder.php
+++ database/seeds/ApplicationTypeSeeder.php
@@ -14,7 +14,7 @@ class ApplicationTypeSeeder extends Seeder
     {
         DB::table('application_types')->insert([
             'name' => 'template1',
-            'signer_orders' => json_encode([3, 2]),
+            'signer_orders' => json_encode([1, 3]),
             'description' => 'Справка по месту требования'
         ]);
     }
--- migrate_db_and_seed.sh
+++ migrate_db_and_seed.sh
@@ -2,6 +2,6 @@
 
 php artisan migrate:fresh
 composer dump-autoload
-php artisan permissions:generate
 php artisan db:seed
+php artisan permissions:generate
 php artisan passport:client --personal -n

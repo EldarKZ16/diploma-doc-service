1ebf7b7aee0a4c3a86940ea925d8a5b0ab6c7dab
jumatinov <heckfy996@gmail.com>
2021-05-01 20:41:13 +0600

    added ew template

---
+++ resources/views/template5.blade.php
@@ -0,0 +1,48 @@
+<html>
+<head><meta http-equiv=Content-Type content="text/html; charset=UTF-8">
+    <style type="text/css">
+        <!--
+        span.cls_002{font-family:"DejaVu Serif",serif;font-size:10.6px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+        div.cls_002{font-family:"DejaVu Serif",serif;font-size:10.6px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+        span.cls_003{font-family:"DejaVu Serif",serif;font-size:13.6px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+        div.cls_003{font-family:"DejaVu Serif",serif;font-size:13.6px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+        span.cls_004{font-family:"DejaVu Serif Bold",serif;font-size:10.6px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
+        div.cls_004{font-family:"DejaVu Serif Bold",serif;font-size:10.6px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
+        span.cls_005{font-family:"DejaVu Serif",serif;font-size:8.2px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+        div.cls_005{font-family:"DejaVu Serif",serif;font-size:8.2px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+        -->
+    </style>
+    <script type="text/javascript" src="07e986f2-aa8a-11eb-8b25-0cc47a792c0a_id_07e986f2-aa8a-11eb-8b25-0cc47a792c0a_files/wz_jsgraphics.js"></script>
+</head>
+<body>
+<divtop:0px;width:595px;height:841px;border-style:outset;overflow:hidden">
+    <divimg src="07e986f2-aa8a-11eb-8b25-0cc47a792c0a_id_07e986f2-aa8a-11eb-8b25-0cc47a792c0a_files/background1.jpg" width=595 height=841></div>
+    <div class="cls_002"><span class="cls_002">Приложение 8</span></div>
+    <div class="cls_002"><span class="cls_002">к Правилам назначения и выплаты</span></div>
+    <div class="cls_002"><span class="cls_002">специального государственного пособия</span></div>
+    <div class="cls_003"><span class="cls_003">СПРАВКА</span></div>
+    <div class="cls_002"><span class="cls_002">Дана гр. </span><span class="cls_004">Дәурен Сабыров Болатұлы 01.09.1999</span><span class="cls_002"> г.р.</span></div>
+    <div class="cls_005"><span class="cls_005">(Ф.И.О. обучающегося/студента, с указанием года рождения)</span></div>
+    <div class="cls_002"><span class="cls_002">В том, что он(а) действительно является обучающимся</span></div>
+    <div class="cls_002"><span class="cls_002">АО Международного университета информационных технологий</span></div>
+    <div class="cls_002"><span class="cls_002">«5B100200»-Системы информационной безопасности</span></div>
+    <div class="cls_002"><span class="cls_002">Госуд.лицензия Серия АБ № 0064060 от 29.05.2009 год без ограничения срока</span></div>
+    <div class="cls_002"><span class="cls_002">4 класса/курса, форма обучения-очное.</span></div>
+    <div class="cls_002"><span class="cls_002">Справка действительна на 2021 - 2022 учебный год, с 1 сентября 2018 г. по 31</span></div>
+    <div class="cls_002"><span class="cls_002">августа 2022 г.</span></div>
+    <div class="cls_002"><span class="cls_002">Справка выдана для предъявления в отделение</span></div>
+    <div class="cls_002"><span class="cls_002">Государственной корпораций</span></div>
+    <div class="cls_002"><span class="cls_002">Срок обучения в учебном заведении 4 года</span></div>
+    <div class="cls_002"><span class="cls_002">Период обучения с 14.08.2018 г. по 04.09.2022 г.</span></div>
+    <div class="cls_004"><span class="cls_004">Примечание:</span><span class="cls_002"> справка действительна 1 год.</span></div>
+    <div class="cls_002"><span class="cls_002">В случае отчисления обучающегося из учебного заведения или перевода на</span></div>
+    <div class="cls_002"><span class="cls_002">заочную форму обучения, руководитель учебного заведения извещает отделение</span></div>
+    <div class="cls_002"><span class="cls_002">Государственной корпораций по месту жительства получателя пособия.</span></div>
+    <div class="cls_004"><span class="cls_004">Проректор по АиВД</span></div>
+    <div class="cls_004"><span class="cls_004">Умаров Тимур Фаридович</span></div>
+    <div class="cls_005"><span class="cls_005">Исп: Самат А.</span></div>
+    <div class="cls_005"><span class="cls_005">Тел.: 330-85-67 (внут.2063)</span></div>
+</div>
+
+</body>
+</html>
